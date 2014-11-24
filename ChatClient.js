4eec89afcd3d315e26edd6d2941fc3fedacbbb1a
Shawn Hussey <shawnhussey+github@gmail.com>
2014-11-23 20:59:01 -0330

    Converted logic to angular controller

--- ChatClient.js
+++ ChatClient.js
@@ -6,112 +6,117 @@
         $scope.debugIt = function() {
             console.debug($scope);
         };
-    });
 
-    var currentUser;
-	var history = [];
-	
-	/**
-	 *	Send a message to the server.
-	 */
-	function sendMessage(sender, body, date) {
-		$.post(
-            './cgi/addMessage',
-            JSON.stringify({
-                sender: sender,
-                body: body,
-                date: date
-            }),
-            function(data, status) {
 
+        ////////////////////////
+
+        $scope.currentUser = null;
+    	$scope.history = [];
+    	
+    	/**
+    	 *	Send a message to the server.
+    	 */
+    	$scope.sendMessage = function sendMessage(sender, body, date) {
+    		$.post(
+                './cgi/addMessage',
+                JSON.stringify({
+                    sender: sender,
+                    body: body,
+                    date: date
+                }),
+                function(data, status) {
+
+                }
+            );
+    	};
+    	
+    	/**
+    	 *	Get messages from the server.
+    	 */
+    	$scope.getMessages = function getMessages(callback) {
+    		$.post('./cgi/getMessages', function(messages) {
+    			callback(messages);
+    		}, 'json');
+    	};
+        
+    	/**
+    	 *	Get the current user for this address, if exists.
+    	 */
+    	$scope.getCurrentUser = function getCurrentUser(callback) {
+    		$.post('./cgi/getCurrentUser', function(data) {
+    			callback(data.user);
+    		}, 'json');
+    	};
+        
+        /**
+         *  Attempt to create a new user associated with this address.
+         */
+        $scope.createNewUser = function createNewUser(username, callback) {
+            var sendData = {username: username};
+            $.post('./cgi/createNewUser', JSON.stringify(sendData), function(data) {
+                //TODO
+    			
+    			callback(data);
+    		}, 'json');
+        };
+    	
+    	$scope.refreshHistory = function refreshHistory(callback) {
+    		$scope.getMessages(function (messages) {
+                var messagesString = "";
+                for (var key in messages) {
+                    var prepend = (messagesString ? '\n' : ''); //prepend newline for all but first message
+                    var message = messages[key];
+                    $scope.history = messages;
+                    messagesString += prepend + message.sender + ': ' + message.body;
+                };
+                $('#historyArea').html(messagesString);
+                if (callback) {
+                    callback(messages);
+                }
+    		});
+    	};
+    	
+        // get an old or new username 
+        $scope.getCurrentUser(function(username) {
+            if (!username) {
+                var desiredName;
+                while (!desiredName) {
+                    desiredName = prompt('No username associated with this address. Please indicate desired nickname:');
+                }
+                $scope.createNewUser(desiredName, function(data) {
+                    $scope.currentUser = data.username;
+                });
+                return;
             }
-        );
-	}
-	
-	/**
-	 *	Get messages from the server.
-	 */
-	function getMessages(callback) {
-		$.post('./cgi/getMessages', function(messages) {
-			callback(messages);
-		}, 'json');
-	}
-    
-	/**
-	 *	Get the current user for this address, if exists.
-	 */
-	function getCurrentUser(callback) {
-		$.post('./cgi/getCurrentUser', function(data) {
-			callback(data.user);
-		}, 'json');
-	}
-    
-    /**
-     *  Attempt to create a new user associated with this address.
-     */
-    function createNewUser(username, callback) {
-        var sendData = {username: username};
-        $.post('./cgi/createNewUser', JSON.stringify(sendData), function(data) {
-            //TODO
-			
-			callback(data);
-		}, 'json');
-    }
-	
-	function refreshHistory(callback) {
-		getMessages(function (messages) {
-            var messagesString = "";
-            for (var key in messages) {
-                var prepend = (messagesString ? '\n' : ''); //prepend newline for all but first message
-                var message = messages[key];
-                messagesString += prepend + message.sender + ': ' + message.body;
-            };
-            $('#historyArea').html(messagesString);
-            if (callback) {
-                callback(messages);
-            }
-		});
-	}
-	
-    // get an old or new username 
-    getCurrentUser(function(username) {
-        if (!username) {
-            var desiredName;
-            while (!desiredName) {
-                desiredName = prompt('No username associated with this address. Please indicate desired nickname:');
-            }
-            createNewUser(desiredName, function(data) {
-                currentUser = data.username;
-            });
-            return;
-        }
-        currentUser = username;
-    });
-    
-    
-	$(document).ready(function() {
-        function scrollHistoryToEnd() {
-            var $history = $('#historyArea');
-            $history.scrollTop($history[0].scrollHeight - $history.height());
-            $('#inputField').focus();
-        }
-    
-        function triggerSend() {
-            var messageBody = $('#inputField').val();
-            $('#inputField').val('');
-			sendMessage(currentUser, messageBody, new Date().getTime());
-			refreshHistory(scrollHistoryToEnd);
-        }
-		$('#sendButton').click(function() {
-			triggerSend();
-		});
-        $('#inputField').keypress(function (event) {
-            if (event.which == 13) {
-                triggerSend();
-            }
+            $scope.currentUser = username;
         });
         
-        refreshHistory(scrollHistoryToEnd);
-        setInterval(refreshHistory, 250);
-	});
+        
+    	$scope.init = function init() {
+            function scrollHistoryToEnd() {
+                var $history = $('#historyArea');
+                $history.scrollTop($history[0].scrollHeight - $history.height());
+                $('#inputField').focus();
+            }
+        
+            function triggerSend() {
+                var messageBody = $('#inputField').val();
+                $('#inputField').val('');
+    			$scope.sendMessage($scope.currentUser, messageBody, new Date().getTime());
+    			$scope.refreshHistory(scrollHistoryToEnd);
+            }
+    		$('#sendButton').click(function() {
+    			triggerSend();
+    		});
+            $('#inputField').keypress(function (event) {
+                if (event.which == 13) {
+                    triggerSend();
+                }
+            });
+            
+            $scope.refreshHistory(scrollHistoryToEnd);
+            setInterval($scope.refreshHistory, 250);
+    	};
+
+    });
 //})();
